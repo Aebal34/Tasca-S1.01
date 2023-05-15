f4280f0235f0824fe77c44150339bf50f31ee4c2
Joren <97386268+Aebal34@users.noreply.github.com>
2023-05-15 13:21:45 +0200

    Al llamar método estático, el bloque no se ejecuta.

--- n1exercici1/WindInstrument.java
+++ n1exercici1/WindInstrument.java
@@ -7,6 +7,17 @@ public class WindInstrument extends Instrument{
 		super(name, price);
 	}
 	
+	//BLOCK
+	{
+		play(); //Every time we instantiate the object
+	}
+	
+	//METHODS
+	
+	static void test() {  //Just to instantiate the class without creating an object
+		System.out.println("Pruebas el instrumento. Suena bien.");
+	}
+	
 	public void play() {
 		
 		System.out.println("Suena un instrumento de viento.");
