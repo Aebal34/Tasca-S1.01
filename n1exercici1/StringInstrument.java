ada9d9644ef1596b6d2aaa6ba7a11009910fdf42
Joren <97386268+Aebal34@users.noreply.github.com>
2023-05-15 13:17:59 +0200

    Bloques estáticos al llamar método estático

--- n1exercici1/StringInstrument.java
+++ n1exercici1/StringInstrument.java
@@ -7,6 +7,18 @@ public class StringInstrument extends Instrument{
 		super(name, price);
 	}
 	
+	//STATIC BLOCK
+	
+	static {
+		System.out.println("Pruebas el instrumento.");
+	}
+	
+	//METHODS
+	
+	static void test() {  //Just to instantiate the class without creating an object
+		System.out.println("Suena bien.");
+	}
+	
 	public void play() {
 		
 		System.out.println("Suena un instrumento de cuerda.");
